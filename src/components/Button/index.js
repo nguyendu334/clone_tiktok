9787121ae3f97571db51495e16d746e468b34870
_nguyen.du <1851061329@e.tlu.edu.vn>
2023-02-11 11:03:28 +0700

    setup babel-plugin-module-resolver

---
+++ src/components/Button/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import myAlert from '~/myAlert'
+
+export default function index() {
+  return (
+    <button onClick={myAlert}>Click me</button>
+  )
+}
---
+++ src/myAlert.js
@@ -0,0 +1,6 @@
+const myAlert = () => {
+    alert('Hello World!');
+
+}
+
+export default myAlert;

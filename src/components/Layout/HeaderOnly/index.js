d968e265c15bcd6b599a0a547e456e6e242ae12d
_nguyen.du <1851061329@e.tlu.edu.vn>
2023-02-11 14:10:21 +0700

    Setup Router/Layout

---
+++ src/components/Layout/HeaderOnly/index.js
@@ -0,0 +1,13 @@
+import Header from '~/components/Layout/components/Header'
+
+export default function DefaultLayout({ children }) {
+
+  return (
+    <div>
+        <Header />
+        <div className='container'>
+            <div className='content'>{ children }</div>
+        </div>
+    </div>
+  )
+}
